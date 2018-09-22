05bafb89cf88cea4453d18fd3b243381d74b706d
이진석 <sonaky47@gmail.com>
2018-09-22 15:16:59 +0900

    포스트 리스트 만들기, 포스트 카테고리 만들기

--- back-end/myblog/app/models.py
+++ back-end/myblog/app/models.py
@@ -2,8 +2,15 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class PostCategory(models.Model):
+  display_name = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.display_name
+
 class Post (models.Model):
   author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+  category = models.ForeignKey(PostCategory, on_delete=models.DO_NOTHING)
   title = models.CharField(max_length=200)
   created_date = models.DateTimeField(default=timezone.now)
   published_date = models.DateTimeField(blank=True, null=True)
@@ -14,3 +21,4 @@ class Post (models.Model):
   
   def __str__(self):
     return self.title
+
--- back-end/myblog/app/views.py
+++ back-end/myblog/app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect, redirect
-from .forms import LoginForm
 from django.contrib.auth import login, authenticate
 from django.urls import reverse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .forms import LoginForm
+from .models import Post
 
 # view__index
 def view__index(request):
@@ -22,17 +24,28 @@ def view__index(request):
   return render(request, 'app/index.html', context)  
 
 # view_home
-def view__home(request):
+def view__posts(request):
+  post_list = Post.objects.all().values(
+    'id',
+    'title',
+    'created_date',
+    'category_id',
+    'category__display_name'
+  )
+  paginator = Paginator(post_list, 20)
+  page = request.GET.get('page') if request.GET.get('page') else 1  
+  posts = paginator.get_page(page)
   context = {
     'css': [
-      'app/css/home.css'
+      'app/css/posts.css'
     ],
     'js': [
-      'app/js/home.js'
+      'app/js/posts.js'
     ],
-    'request': request
+    'request': request,
+    'posts': posts
   }
-  return render(request, 'app/home.html', context)
+  return render(request, 'app/posts.html', context)
 
 # api__login
 def api__login(request): 
@@ -43,6 +56,6 @@ def api__login(request):
     user = authenticate(username = username, password = password)
     if user is not None:
       login(request, user)
-      return redirect('app:home')
+      return redirect('app:posts')
     else:
       return HttpResponse('로그인 실패. 다시 시도 해보세요.')
