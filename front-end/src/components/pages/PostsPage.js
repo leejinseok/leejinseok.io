2fd9ac2cb36ccc4eb3a3904c7ece28fee0b9b39c
Jinseok Lee <sonaky47@gmail.com>
2018-10-14 11:40:36 +0900

    django posts get method def update, react axios get update

--- front-end/src/components/pages/PostsPage.js
+++ front-end/src/components/pages/PostsPage.js
@@ -3,15 +3,19 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import axios from 'axios';
 import '../../static/css/posts.css';
+const instance = axios.create({
+  baseURL: 'http://localhost:8000/app',
+  timeout: 1000,
+});
 
 const Posts = ({posts}) => {
   return posts.map((post, index) => {
     return (
       <article key={index}>
         <h2>{post.title}</h2>
-        <p>
-          {post.body}
+        <p dangerouslySetInnerHTML={{ __html: post.content }}>
         </p>
+
       </article>
     )
   })
@@ -23,11 +27,13 @@ class PostsPage extends Component {
   }
 
   componentDidMount () {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => {
+    instance.get('/api/posts')
+      .then(response => {
         this.setState({
           posts: response.data
         })
+      }).catch(err => {
+        console.error(err);
       })
   }
 
